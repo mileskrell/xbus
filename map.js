3bb4b24dc059afb4d0cd6d27dfb8336b23b670d0
Miles Krell <noreply@mileskrell.com>
2021-12-26 20:22:31 -0500

    Show vehicles with correct heading and color (no animation)

--- map.js
+++ map.js
@@ -2,7 +2,15 @@ const xMin = -75.56;
 const yMin = 38.92;
 const xMax = -73.89;
 const yMax = 41.36;
+const translocRequestInit = {
+    method: 'GET',
+    headers: {
+        'x-rapidapi-host': 'transloc-api-1-2.p.rapidapi.com',
+        'x-rapidapi-key': '1b6e9ed24cmsh2d1311786670ae8p1eace0jsncef54644775d',
+    },
+};
 mapboxgl.accessToken = 'pk.eyJ1IjoibWlsZXNrcmVsbCIsImEiOiJja3hqNXlmY2gzazEyMnRxaDA1Y3J2MjJzIn0.Uz5PQwiiTDyv3fr8YTTwpA';
+let routes, stops, vehicles;
 const map = new mapboxgl.Map({
     container: 'map', // container ID
     style: 'mapbox://styles/mileskrell/ckxl9zz5632ey14oafkathv0c', // style URL
@@ -76,6 +84,10 @@ function setSelectedPlace(placeType, feature, lngLat) {
 }
 
 map.on('load', () => {
+    map.loadImage('navigation.png', (error, image) => {
+        if (error) throw error;
+        return map.addImage('vehicle', image, {sdf: true});
+    });
     map.removeLayer('transit-label') // Remove layer for Mapbox Streets bus stop icons
     map.addSource('selected place', {type: 'geojson', data: {type: 'Feature'}});
     map.addLayer({
@@ -84,10 +96,51 @@ map.on('load', () => {
         source: 'selected place',
         paint: {'fill-color': '#cc0033'},
     });
+    map.addSource('vehicles', {type: 'geojson', data: {type: 'Feature'}});
+    map.addLayer({
+        id: 'vehicles',
+        type: 'symbol',
+        source: 'vehicles',
+        paint: {'icon-color': ['get', 'route_color']},
+        layout: {'icon-image': 'vehicle', 'icon-rotate': ['get', 'heading']}
+    });
     map.on('mouseleave', 'Rutgers parking lots', () => map.getCanvas().style.cursor = '');
     map.on('mouseleave', 'Rutgers buildings', () => map.getCanvas().style.cursor = '');
     map.on('mouseenter', 'Rutgers parking lots', () => map.getCanvas().style.cursor = 'pointer');
     map.on('mouseenter', 'Rutgers buildings', () => map.getCanvas().style.cursor = 'pointer');
     map.on('click', 'Rutgers parking lots', e => setSelectedPlace('lot', e.features[0], e.lngLat));
     map.on('click', 'Rutgers buildings', e => setSelectedPlace('building', e.features[0], e.lngLat));
+
+    async function fetchBusStuff() {
+        routes = (await (await fetch('https://transloc-api-1-2.p.rapidapi.com/routes.json?agencies=1323', translocRequestInit)).json())['data'][1323];
+        stops = (await (await fetch("https://transloc-api-1-2.p.rapidapi.com/stops.json?agencies=1323", translocRequestInit)).json())['data'];
+        vehicles = (await (await fetch("https://transloc-api-1-2.p.rapidapi.com/vehicles.json?agencies=1323", translocRequestInit)).json())['data'][1323];
+        console.log("fetched routes, stops, and vehicles");
+
+        const routeIdToRouteMap = {};
+        routes.forEach(route => routeIdToRouteMap[route.route_id] = route);
+        const stopIdToStopMap = {};
+        stops.forEach(stop => stopIdToStopMap[stop.stop_id] = stop);
+        const vehicleIdToVehicleMap = {};
+        vehicles.forEach(vehicle => vehicleIdToVehicleMap[vehicle.vehicle_id] = vehicle);
+
+        vehicles.forEach(vehicle => vehicle.route = routeIdToRouteMap[vehicle.route_id]);
+        const vehicleFeatures = vehicles.map(vehicle => ({
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [vehicle.location.lng, vehicle.location.lat],
+            },
+            properties: {
+                vehicle_id: vehicle.vehicle_id,
+                heading: vehicle.heading,
+                route_color: '#' + vehicle.route.color,
+            },
+        }));
+        map.getSource('vehicles').setData({type: 'FeatureCollection', features: vehicleFeatures});
+
+        setTimeout(fetchBusStuff, 5000);
+    }
+
+    fetchBusStuff();
 });
